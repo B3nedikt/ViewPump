6aec369875a74eea26c1c11d7d53b7a3e3acbb74
B3nedikt <mail@b3nedikt.dev>
2023-07-27 21:15:21 +0200

    Fixed a issue with web views crashing in specific android versions

---
+++ viewpump/src/main/java/dev/b3nedikt/viewpump/WebViewContextWrapper.kt
@@ -0,0 +1,12 @@
+package dev.b3nedikt.viewpump
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Resources
+
+internal class WebViewContextWrapper(baseContext: Context) : ContextWrapper(baseContext) {
+
+    override fun getResources(): Resources {
+        return applicationContext.resources
+    }
+}
