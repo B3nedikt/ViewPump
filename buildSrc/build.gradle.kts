6e641757a667514df0c937f914f2f5c9d9b7ab84
B3nedikt <mail@b3nedikt.dev>
2023-07-29 12:56:19 +0200

    Fixed issues with the publication script

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,5 +11,5 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:8.1.0")
     implementation(kotlin("gradle-plugin", "1.9.0"))
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
 }
